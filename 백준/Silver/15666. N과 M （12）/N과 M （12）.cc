c79443418035f80e35b436944e850fcf7d8bd2e8
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-02-03 16:25:59 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int N, M;
+vector<int> arr;
+vector<int>sv;
+
+void gt(int N, int M,  int current) {
+  
+    if (arr.size() == M)
+    {
+        for (int num : arr) {
+            cout << num << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    int check = 0;
+    for (int i = current; i <= N; ++i) {
+        if (check != sv[i]) {
+            check = sv[i];
+
+            arr.push_back(sv[i]);
+            gt(N, M, i);
+          
+            arr.pop_back();
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    sv.push_back(0);
+    int s;
+    cin >> N>>M;
+    for (int i = 1; i <= N; i++) {
+        cin >> s;
+       
+        sv.push_back(s);
+    }
+    sort(sv.begin(), sv.end());
+  
+    gt(N, M, 1);
+
+    return 0;
+}
+
+
+
