e5b079cbb951be4281c28c32920bc0fe1759db95
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-03-19 14:51:46 +0900

    [Gold V] Title: 다각형의 면적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -1,8 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int N;
+int N, K, M;
+
 
 int main() {
     ios::sync_with_stdio(false);
@@ -10,18 +13,22 @@ int main() {
     cout.tie(NULL);
 
     long double sum = 0;
-    scanf("%d", &N);
+    cin >> N;
     N -= 2;
     long long x1, y1, x2, y2;
-    scanf("%lld %lld", &x1, &y1);
-    scanf("%lld %lld", &x2, &y2);
-
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
     while (N--) {
         long long x3, y3;
-        scanf("%lld %lld", &x3, &y3);
+        cin >> x3 >> y3;
         sum += (x1 * y2 + x2 * y3 + x3 * y1 - x2 * y1 - x3 * y2 - x1 * y3) / 2.0;
+
         x2 = x3; y2 = y3;
+
     }
     printf("%.1Lf", fabsl(sum));
     return 0;
 }
+
+
+
