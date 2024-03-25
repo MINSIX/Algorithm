dc75f69d6dc1a186a74be7614663680a7a63ff6b
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-03-25 16:37:22 +0900

    [Gold IV] Title: RGB거리 2, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -1,48 +1,47 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<queue>
+#include<bits/stdc++.h>
 using namespace std;
-
-#define MAX 1000000
+const int INF = 1000000;
 int N;
-int color[1001][3];
-int dp[1001][3];
-int ans = MAX;
+int home[1001][3] = { 0, };
+int dp[1001][3] = { 0, };
+int result = INF;
+int main() {
+    
+    cin >> N;
+   
+    for (int i = 1; i <= N; i++) {
+        cin >> home[i][0] >> home[i][1] >> home[i][2];
+    }
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
 
-	cin >> N;
+    for (int rgb = 0; rgb <= 2; rgb++) {
 
-	for (int i = 1; i <= N; i++)
-		cin >> color[i][0] >> color[i][1] >> color[i][2];
+        for (int i = 0; i <= 2; i++)
+        {
+            if (i == rgb)dp[1][i] = home[1][i];
+            else dp[1][i] = INF;
+        }
 
-	for (int RGB = 0; RGB <= 2; RGB++) // RGB : 1번 집의 색
-	{
-		// 1번집에 지정된 색 이외의 색은 MAX로 지정하여 dp할때 선택되지 않도록 함
-		for (int i = 0; i <= 2; i++) 
-		{
-			if (i == RGB) dp[1][i] = color[1][i];
-			else dp[1][i] = MAX;
-		}
+  
+        for (int i = 2; i <= N; i++) {
 
-		// DP
-		for (int i = 2; i <= N; i++)
-		{
-			dp[i][0] = color[i][0] + min(dp[i - 1][1], dp[i - 1][2]);
-			dp[i][1] = color[i][1] + min(dp[i - 1][0], dp[i - 1][2]);
-			dp[i][2] = color[i][2] + min(dp[i - 1][0], dp[i - 1][1]);
-		}
+            dp[i][0] = home[i][0] + min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = home[i][1] + min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] = home[i][2] + min(dp[i - 1][1], dp[i - 1][0]);
 
-		// 1번집 색과 N번집 색이 겹치지 않게 함
-		for (int i = 0; i <= 2; i++)
-		{
-			if (i == RGB) continue;
-			else ans = min(ans, dp[N][i]);
-		}
-	}
-	cout << ans;
+        }
+
+        for (int i = 0; i <= 2; i++)
+        {
+            if (i == rgb)continue;
+            else result = min(result, dp[N][i]);
+        }
+
+    }
+
+   cout << result << endl;
+    
 }
+
+
+
