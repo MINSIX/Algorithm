f3c67f0617fd933fb752a6d5e16949e151d49a6e
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-05-06 19:14:17 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int X, Y, D, T;
+
+const double INF = 1e9;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> X;
+    while (X--)
+        cout << 's';
+
+
+
+
+
+
+
+
+
+}
