059d428b93d4df06c4057dbff66230a860168cfe
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-05-06 19:43:29 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int X, Y, D, T;
+
+const double INF = 1e9;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+
+    cin >> X;
+    
+    cout << 2000 << '\n';
+    for (int i = 0; i < 1000; i++) cout << 1 << ' ';
+    for (int i = 0; i < 1000; i++) cout << 1000 << ' ';
+
+
+
+}
