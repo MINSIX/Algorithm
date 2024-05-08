6e46f78df16f6aee915c6c21e52a8c13f64a289f
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-05-08 09:28:15 +0900

    [Gold III] Title: 약수 지우기 게임 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12107. 약수 지우기 게임 1/약수 지우기 게임 1.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int T;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> T;
+    if (T == 1)
+        cout << "B" << endl;
+    else
+        cout << "A" << endl;
+
+
+
+
+}
