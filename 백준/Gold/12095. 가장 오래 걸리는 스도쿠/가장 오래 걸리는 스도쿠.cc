d06fa333b20d93718246fce8d9d1ebd499f0544c
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-05-12 21:07:24 +0900

    [Gold III] Title: 가장 오래 걸리는 스도쿠, Time: 0 ms, Memory: 2020 KB, Score: 82 point -BaekjoonHub

---
+++ 백준/Gold/12095. 가장 오래 걸리는 스도쿠/가장 오래 걸리는 스도쿠.cc
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << "0 3 5 4 6 9 2 7 8" << endl;
+    cout << "7 8 2 1 0 5 6 0 9" << endl;
+    cout << "0 6 0 2 7 8 1 3 5" << endl;
+    cout << "3 2 1 0 4 6 8 9 7" << endl;
+   cout<<"8 0 4 9 1 3 5 0 6" << endl;
+   cout << "5 9 6 8 2 0 4 1 3" << endl;
+   cout << "9 1 7 6 5 2 0 8 0" << endl;
+   cout << "6 0 3 7 0 1 9 5 2" << endl;
+   cout << "2 5 8 3 9 4 7 6 0" << endl;
+ 
+    return 0;
+}
