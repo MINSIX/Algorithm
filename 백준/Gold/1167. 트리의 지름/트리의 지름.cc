f0efae58973ae5309e2ade86c656ff956030a140
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-05-20 09:47:21 +0900

    [Gold II] Title: 트리의 지름, Time: 100 ms, Memory: 11400 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,85 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int N;
+vector<pair<int, short>> node[100001];
+bool visited[100001] = { false, };
+int result=0, edp=0;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    int a, b, c;
+
+
+    for (int i = 0; i < N; i++) {
+        cin >> a >> b >> c;
+
+        node[a].push_back({ b,c });
+        node[b].push_back({ a,c });
+
+        cin >> b;
+        while (b != -1) {
+            cin >> c;
+            node[a].push_back({ b,c });
+            node[b].push_back({ a,c });
+            cin >> b;
+        }
+
+
+    }
+    stack<pair<int, int>> dfs;
+
+    dfs.push({ 1,0 });
+    visited[1] = true;
+    while (!dfs.empty())
+    {
+        int now = dfs.top().first;
+        int len = dfs.top().second;
+        dfs.pop();
+        visited[now] = true;
+
+        if (result < len) {
+            result = len;
+            edp = now;
+        }
+        
+        for (int i = 0; i < node[now].size(); i++) {
+          if(!visited[node[now][i].first])
+            dfs.push({ node[now][i].first,len + node[now][i].second });
+
+
+
+        }
+
+    }
+    result = 0;
+    memset(visited, false, sizeof(visited));
+    dfs.push({ edp,0 });
+    visited[edp] = true;
+    while (!dfs.empty())
+    {
+        int now = dfs.top().first;
+        int len = dfs.top().second;
+        dfs.pop();
+
+
+    
+        visited[now] = true;
+        if (result < len) {
+            result = len;
+            edp = now;
+        }
+
+        for (int i = 0; i < node[now].size(); i++) {
+            if (!visited[node[now][i].first])
+            dfs.push({ node[now][i].first,len + node[now][i].second });
+
+
+
+        }
+
+    }
+    cout << result << endl;
+    return 0;}
