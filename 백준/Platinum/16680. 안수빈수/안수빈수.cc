fcb43bec8a06a19db6c6f64ab8c13a26487a791f
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-05-23 12:49:55 +0900

    [Platinum V] Title: 안수빈수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/16680. 안수빈수/안수빈수.cc
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+int T;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> T;
+    while (T--) {
+        long long N;
+        cin >> N;
+        cout << (N * ((int)1e9 - 1)) << "\n";
+
+    }
+
+
+
+}
