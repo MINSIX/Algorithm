05b3c3686b083f3a2002f6a41a861c516f4fd4f4
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-05-26 14:58:31 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+    sort(arr.begin(), arr.end());
+
+    int stst, eded;
+    int nowval = 2000000001;
+      int st = 0;
+        int ed = N - 1;
+        while (st < ed) {
+            int sum = arr[st] + arr[ed];
+            if (nowval > abs(sum)) {
+                nowval=abs(sum);
+                    stst=arr[st];
+                    eded=arr[ed];
+            }
+            if(sum>0)
+                ed--;
+            else
+                st++;
+
+        }
+        cout << stst << " " << eded;
+
+
+}
