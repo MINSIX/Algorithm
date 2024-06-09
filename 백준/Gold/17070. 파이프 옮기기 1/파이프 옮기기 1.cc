8854273cc830c346ad0ea2dc4803f5c025b0ac54
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-06-09 18:38:52 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 88 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -11,42 +11,70 @@ int main() {
     int N;
     cin >> N;
 
-    vector<vector<int>> arr(N + 1, vector<int>(N + 1));
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
+
+    vector<vector<int>>arr(N, vector<int>(N));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             cin >> arr[i][j];
+
+
         }
     }
+    if (arr[N - 1][N - 1] == 1)
+    {       cout << 0 << endl;
+    return 0;
+}int cnt = 0;
 
-    int cnt = 0;
-
-    queue<pair<pair<int, int>, int>> q;
-    q.push({ {1, 2}, 0 });
+    queue<pair<pair<int, int>, int>>q;
+    q.push({ { 0,1 }, 0});
     while (!q.empty()) {
         int nowx = q.front().first.first;
         int nowy = q.front().first.second;
         int di = q.front().second;
         q.pop();
-
         for (int i = 0; i < 3; i++) {
-            if (di == 0 && i == 1) continue;
-            if (di == 1 && i == 0) continue;
-
+            if(di==0)
+                if (i == 1)
+                    continue;
+            
+            if (di == 1)
+                if (i == 0)
+                    continue;
+            
+            
             int xx = nowx + dx[i];
             int yy = nowy + dy[i];
-            if (xx > N || yy > N) continue;
-            if (arr[xx][yy] == 1) continue;
-            if (i == 2 && (arr[xx][nowy] == 1 || arr[nowx][yy] == 1)) continue;
 
-            if (xx == N && yy == N) {
+
+         
+
+            if (xx >= N || yy >= N)
+                continue;
+            if (arr[xx][yy] == 1)
+                continue;
+
+            if (i == 2)
+                if (arr[nowx + 1][nowy] == 1 || arr[nowx][nowy + 1] == 1)
+                    continue;
+          
+
+               if (xx == N - 1 && yy == N - 1)
+            {
                 cnt++;
                 continue;
             }
 
-            q.push({ {xx, yy}, i });
+
+            q.push({ { xx, yy }, i });
         }
+
     }
+
+
+
     cout << cnt << endl;
+
+
 
     return 0;
 }
