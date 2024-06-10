25c35089d799e263ad77d01e4e4b85a7f0ad51d6
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-06-10 10:22:44 +0900

    [Gold II] Title: 문제집, Time: 28 ms, Memory: 3964 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.cc
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -4,25 +4,23 @@ int N, M;
 vector<int> indegree(32001, 0);
 void topologicalSort(vector<vector<int>>& graph) {
 
-    priority_queue<int> q; // 진입 차수가 0인 정점을 저장하는 큐
+    priority_queue<int> q; 
 
   
 
-    // 진입 차수가 0인 정점을 큐에 추가
-    //-를 넣어서 우선순위를 높게높게
     for (int i = 1; i <= N; ++i) {
         if (indegree[i] == 0) {
             q.push(-i);
         }
     }
 
-    // 위상 정렬 수행
+
     while (!q.empty()) {
         int u = -q.top();
         q.pop();
          cout << u << " ";
 
-        // 현재 정점과 연결된 간선 제거
+  
          for (int i = 0; i < graph[u].size(); i++) {
              int next = graph[u][i];
              indegree[next]--;
@@ -44,9 +42,7 @@ int main() {
         graph[a].push_back(b);
         indegree[b]++;
     }
-    for(int i=1;i<=N;i++){
-    sort(graph[i].begin(), graph[i].end());
-    }topologicalSort(graph);
+    topologicalSort(graph);
 
     return 0;
 }
