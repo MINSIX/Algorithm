41b17fb5c9af6e21085593f9b5be69418ab905d9
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-06-16 16:56:14 +0900

    [Gold III] Title: 나이트 게임, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16884. 나이트 게임/나이트 게임.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+
+        if (N %2== 1)
+            cout << "koosaga" << endl;
+        else
+            cout << "cubelover" << endl;
+
+
+
+    }
+
+    return 0;
+}
