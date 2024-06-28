8a93901aaa08b78d99180f11943f378f5b9cdc63
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-06-28 10:08:06 +0900

    [Silver III] Title: 피자 (Large), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14607. 피자 （Large）/피자 （Large）.cc
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long N;
+    cin >> N;
+   
+    cout << (N - 1)*N / 2;
+
+
+
+}
+
+
+
