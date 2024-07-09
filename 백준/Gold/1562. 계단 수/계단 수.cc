07adff7a93a9dae609217b05c22d3522dd216dee
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-07-09 13:12:45 +0900

    [Gold I] Title: 계단 수, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -2,40 +2,40 @@
 using namespace std;
 
 const long long MOD = 1000000000;
-long long dp[101][10][1024] = {0,};
+long long dp[101][10][4] = {0,};
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+  
     int N;
     cin >> N;
-    
-    for (int i = 1; i <= 9; i++) {
-        dp[1][i][1 << i] = 1;
+    long long cnt = 0;
+  
+    for(int i = 1; i <= 9; i++) {
+        dp[1][i][2*(i==9)] = 1;
     }
-    
-    for (int i = 2; i <= N; i++) {
-        for (int j = 0; j <= 9; j++) {
-            for (int k = 0; k < 1024; k++) {
-                if (j == 0) {
-                    dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i-1][j+1][k]) % MOD;
-                } else if (j == 9) {
-                    dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i-1][j-1][k]) % MOD;
-                } else {
-                    dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i-1][j-1][k] + dp[i-1][j+1][k]) % MOD;
+  
+    for(int i = 2; i <= N; i++) {
+        for(int j = 0; j <= 9; j++) {
+            for(int k = 0; k < 4; k++) {
+              
+                  if(j-1>=0)
+                    dp[i][j-1][k|(j==1)] +=dp[i-1][j][k] % MOD;
+                
+                if(j+1 <= 9) {
+                    dp[i][j+1][k|2*(j==8)]+= dp[i-1][j][k] % MOD;
                 }
             }
         }
     }
-    
-    long long cnt = 0;
-    for (int j = 0; j <= 9; j++) {
-        cnt = (cnt + dp[N][j][1023]) % MOD;
+  
+    for(int i = 0; i <= 9; i++) {
+        cnt += dp[N][i][3] ;
+        cnt%=MOD;
     }
-    
-    cout << cnt << endl;
-    
+  
+    cout << cnt;
     return 0;
 }
