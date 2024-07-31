d52888e9ce13f17b5d30dcba3cd2e338826962be
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-07-31 10:31:56 +0900

    [Gold IV] Title: 데이터 만들기 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/7140. 데이터 만들기 1/데이터 만들기 1.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  
+  cout << 103<<endl;
+  for(int i=0;i<103;i++)
+  cout <<0<<endl;
+  cout <<1<<endl;
+  cout<<1<<" "<<0;
+  
+  
+  
+  
+  
+}
