b52fa8a0049bc2c11e1fb4409e6c8dd442d91957
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-08-27 11:01:27 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int k=0, d=0, a=0;
+      scanf("%d/%d/%d",&k,&d,&a);
+    if((k+a)<d)
+        cout << "hasu";
+    else if(d==0)
+        cout<<"hasu";
+    else
+        cout <<"gosu";
+    
+}
