94a7e59c36ef692f4a888a8a6877218c03562e46
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-12-09 11:03:00 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int N;
+  cin >> N;
+  for(int i=0;i<N;i+=4)
+  cout <<"long ";
+  cout<<"int";
+  
+
+  
+  
+  
+}
