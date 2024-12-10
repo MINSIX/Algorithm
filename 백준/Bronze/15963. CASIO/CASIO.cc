b9fa4f11d4a9e9410689959ad3b676aa2b6915ee
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-12-10 14:33:19 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+  
+
+    long long N,M;
+    cin >> N >> M;
+    if(N==M)
+    cout << 1;
+    else
+    cout << 0;
+
+
+    
+}
