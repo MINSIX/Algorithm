207cd7a9c37666a771dc47fe5bca9aa7ef13f611
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-12-15 00:43:58 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 28 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  
+  
+ int T;
+ cin >> T;
+ while(T--){
+   string s;
+   cin >> s;
+   if(s.length()>=6&&s.length()<=9)
+   cout << "yes\n";
+   else
+   cout << "no\n";
+ }
+}
+  
