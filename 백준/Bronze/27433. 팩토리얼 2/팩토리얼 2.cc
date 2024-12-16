cb9e078e6172cc996e6ab4237fc9426bb02fc9cf
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-12-16 22:32:18 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int T;
+    cin >>T;
+    long long sum=1;
+    for(int i=1;i<=T;i++)
+    sum*=i;
+    cout << sum;
+    
+    
+}
