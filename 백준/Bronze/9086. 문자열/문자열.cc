9156f54d29c1fbf52806611961569ec0298699d5
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-12-27 18:08:18 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+  
+  int T;
+  cin >>T;
+  while(T--){
+    string s;
+    cin >> s;
+    cout << s[0]<<s[s.length()-1]<<"\n";
+    
+  }
+  
+  
+  
+}
