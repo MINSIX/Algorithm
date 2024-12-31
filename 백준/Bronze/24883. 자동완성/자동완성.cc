f5005583dcced262c2c303d8e112c9a9210c5f22
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-12-31 23:27:27 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char A;
+    cin >> A;
+    if(A=='N'||A=='n')
+        cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+    
+}
