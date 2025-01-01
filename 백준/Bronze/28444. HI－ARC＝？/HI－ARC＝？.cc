e4310cf749c02e04bbb2d2aac5ea25f3a6a5e345
MINSIX <98219599+MINSIX@users.noreply.github.com>
2025-01-02 00:53:43 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  
+  int h,i,a,r,c;
+  
+  cin >>h>>i>>a>>r>>c;
+  
+  
+  int fir = h*i;
+  int sec= a*r*c;
+  cout << fir-sec;
+  
+  
+}
