c1bb0750af6343e9bec2cc7e64efb34051360407
MINSIX <98219599+MINSIX@users.noreply.github.com>
2025-01-02 22:35:15 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  
+  int T,S;
+   cin >> T >>S;
+   
+   if(S){
+   cout << 280;
+   return 0;
+     
+   }
+   if(T>=12&&T<=16)
+   {cout << 320;
+   return 0;
+   }
+   cout <<280;
+   
+   
+}
