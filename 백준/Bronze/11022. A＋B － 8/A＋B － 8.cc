8d5feb93b03aa9dcca03dff9960d276f915872ba
MINSIX <98219599+MINSIX@users.noreply.github.com>
2025-01-05 13:01:01 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int T;
+  cin >>T;
+  for(int i=1;i<=T;i++){
+    int a,b;
+    cin >> a>>b;
+    cout << "Case #"<<i<<": "<<a<<" + "<<b<<" = "<<a+b<<endl;
+  }
+  
+}
