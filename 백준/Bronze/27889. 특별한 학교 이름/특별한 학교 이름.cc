03d9dc464213970c96c757fd30fdbd84902b3c71
MINSIX <98219599+MINSIX@users.noreply.github.com>
2025-01-11 11:56:11 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string S;
+  cin >> S;
+  
+  if(S=="NLCS")
+  {
+    cout << "North London Collegiate School";
+    return 0;
+  }
+  
+  if(S=="BHA")
+  {
+    cout << "Branksome Hall Asia";
+    return 0;
+  }
+  if(S=="KIS")
+  {
+    cout << "Korea International School";
+    return 0;
+  }
+  if(S=="SJA")
+  {
+    cout << "St. Johnsbury Academy";
+    return 0;
+  }
+  
+  
+}
