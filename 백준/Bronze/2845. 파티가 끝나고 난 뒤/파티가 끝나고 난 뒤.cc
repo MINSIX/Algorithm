634aea8ee4234a1d065038d6b7377e43fd4a492a
MINSIX <98219599+MINSIX@users.noreply.github.com>
2025-01-17 06:22:07 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  
+  
+  int L,P;
+  cin >>L>>P;
+  vector<int> arr(5);
+  for(int i=0;i<5;i++)
+  cin >>arr[i];
+  
+  for(int i=0;i<5;i++){
+    cout << arr[i]-L*P<< " ";
+  }
+  
+  
+  
+}
