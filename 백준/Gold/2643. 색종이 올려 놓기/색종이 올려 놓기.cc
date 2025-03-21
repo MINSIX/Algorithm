bb0cd462c061714b6ebc237869dd3d9ba6898813
MINSIX <98219599+MINSIX@users.noreply.github.com>
2025-03-21 09:26:04 +0900

    [Gold IV] Title: 색종이 올려 놓기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2643. 색종이 올려 놓기/색종이 올려 놓기.cc
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool cmp(pair<int,int> &a, pair<int,int>&b){
+  if(a.first!=b.first)
+  return a.first>b.first;
+  
+  return a.second>b.second;
+  
+}
+
+
+int main(){
+  
+  int N;
+  cin >> N;
+  vector<pair<int,int>>arr(N);
+  
+  vector<int>dp(N,1);
+  for(auto &i : arr){
+    cin >>i.first>>i.second;
+    if(i.first<i.second)swap(i.first,i.second);
+    
+    
+  }
+  sort(arr.begin(),arr.end(),cmp);
+  int cnt=0;
+  for(int i=0;i<N;i++){
+
+    for(int j=i+1;j<N;j++){
+      
+     if(arr[j].first <= arr[i].first && arr[j].second <= arr[i].second){
+        
+        dp[j] = max(dp[j], dp[i] + 1);
+        cnt = max(cnt, dp[j]);
+      }
+      
+      
+    }
+  }
+  cout <<cnt;
+}
