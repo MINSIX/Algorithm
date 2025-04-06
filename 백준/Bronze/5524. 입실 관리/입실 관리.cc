13e486c6593d649992d3e5035eacbbb81f563c46
MINSIX <98219599+MINSIX@users.noreply.github.com>
2025-04-06 17:13:20 +0900

    [Bronze IV] Title: 입실 관리, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main() 
+{
+
+int N;
+cin >>N;
+while(N--){
+  string s;
+  cin >> s;
+  
+    for (int i = 0; i < s.size(); i++) {
+        s[i] = tolower(s[i]);
+    }
+    cout << s<<"\n";
+}
+
+
+}
