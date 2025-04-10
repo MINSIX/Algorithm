938f829839a403817ab1acd908ef1def0763e8f6
MINSIX <98219599+MINSIX@users.noreply.github.com>
2025-04-10 23:12:15 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+int N;
+cin >> N;
+cin.ignore();
+for(int i=1;i<=N;i++){
+  string s;
+  getline(cin,s);
+  cout << i<<". "<<s<<"\n";
+}
+  
+  
+  
+}
