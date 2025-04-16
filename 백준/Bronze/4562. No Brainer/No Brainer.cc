9355df9259b8bd5f2b6b02d63006979bb9772273
MINSIX <98219599+MINSIX@users.noreply.github.com>
2025-04-16 16:19:19 +0900

    [Bronze IV] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  
+  int N;
+  cin >> N;
+  while(N--){
+    int a,b;
+    cin >> a>>b;
+    if(a<b)
+    cout << "NO BRAINS\n";
+    else
+    cout << "MMM BRAINS\n";
+    
+  }
+  
+  
+}
