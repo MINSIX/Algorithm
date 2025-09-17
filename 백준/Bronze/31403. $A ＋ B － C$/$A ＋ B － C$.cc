5f6694c380964ae85307074cc129562d3de35f3d
MINSIX <98219599+MINSIX@users.noreply.github.com>
2025-09-17 22:23:20 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  
+  int a,b,c;
+  cin >> a>>b>>c;
+  cout <<a+b-c<<endl;
+  string s=to_string(a)+to_string(b);
+  cout << stoi(s)-c;
+  
+  
+}
